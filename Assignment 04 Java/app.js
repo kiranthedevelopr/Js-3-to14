9488422ae664ec289689a3868b3013a8e184c31c
kiranthedevelopr <kirannasim05@gmail.com>
2023-08-13 07:56:59 -0700

    3 to 14

---
+++ Assignment 04 Java/app.js
@@ -0,0 +1,22 @@
+//Q1: Decleared 03 variables in one staement.
+                  //Answer//
+//let= var1,var2,var3;
+
+//Q2 .Decleard 5 legal and 5 illelegal variables names.
+                 //Ansewr//
+//legal variables.
+// let var1, var_2, Variables,variableEight,var7_;
+
+//Illegeal variables
+//let 12var, const ,$var$,--var,/n;
+
+
+//Q3. Display this in your browser
+                //Answer//
+// let display ="Rules for naming JS variables";
+// let var1= "Variable names can only contain numbers,$ and _. For example $my_1stVariable";
+// let var2= "Variables must begin with a letter, $ and _. For example $name, _name or name";
+// let var3= "Variable names are case sensitive";
+// let var4= "Variable names should not be JS keywords"
+// let var5= "<h1><strong><big>" + display + "</big></strong></h1>" + var1 +"<br>"+ var2 +"<br>"+ var3 +"<br>"+ var4;
+// document.write(var5);
---
+++ Assignment 3 Java/app.js
@@ -0,0 +1,28 @@
+// Java script Assignment 03 //
+//Q#1: Decleared a variable caleed age and assign to it your age.show your age in alert box//
+                // Answer//
+//let age= "I am 25 years old";
+//alert (age);
+
+//Q#2:Decleared a initialize variable to keep track of how many times a visitor has visited a web page.show his/her number of visits on your web page.for example "you have visited this site N times"
+                   //Answer/
+ //let visitors ="you have visited this site N times";
+ //alert(visitors);
+
+
+//Q#3: Decleared a variable called birthyear and assign to it your birth.show the following messages in your browser.
+               //Answer//
+//let birthyear="  1998";
+//document.write("My Birth Year Is " + birthYear + "<br>" +"Datatype of my declared variable is number");
+
+
+// Q#4 A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+//a. Visitor’s name
+//b. Product title
+//c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+                  ///Answer//
+//let VisitorName = "John Doe";
+ //let ProductTitle = "T-shirt";
+ ///let Quantity ="5";
+ //document.write(VisitorName + " ordered " + Quantity + " " + ProductTitle + "(s) on XYZ Clothing store");
